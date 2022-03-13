4b06a6ffc9f63a51d5964496d9c616a740a51ce5
Yoan Bernabeu <yoan.bernabeu@gmail.com>
2022-03-13 14:24:07 +0100

    🐛 FIX: php-cs

--- src/Entity/Job.php
+++ src/Entity/Job.php
@@ -105,6 +105,6 @@ class Job
 
     public function __toString(): string
     {
-        return $this->name . ' (' . $this->app->getName() . ')';
+        return $this->name.' ('.$this->app->getName().')';
     }
 }
